1fb79a0c443db8c29e41ad0d4aa1c02e829fd8dc
Kyle Grande <kyle.g.grande@gmail.com>
2023-04-17 14:41:57 -0500

    aikey newlink

--- src/components/ApiKeyModal.js
+++ src/components/ApiKeyModal.js
@@ -16,7 +16,7 @@ function ApiKeyModal({ isOpen, onClose, onSave }) {
   return (
     <div className="api-key-modal">
       <div className="modal-content">
-        <h2>Enter Your <a href='https://platform.openai.com/account/api-keys'>OpenAI</a> Key</h2>
+        <h2>Enter Your <a href='https://platform.openai.com/account/api-keys' target='_blank' rel='noopener noreferrer'>OpenAI</a> Key</h2>
         <input
           type="password"
           value={apiKey}
